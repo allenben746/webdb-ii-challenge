540fa6b35446802335cd6394609a4479e814d2fe
Ben <allenben@protonmail.com>
2019-09-11 06:22:31 -0500

    Implemented CRUD ops - communicates to DB successfully

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -7,8 +7,27 @@ router.get('/', (req, res) => {
         .then(cars => {
             res.json(cars);
         })
+        //Handles any errors that may occur - will post error in console
         .catch(err => {
-            res.status(500).json({message : 'Failed to find cars:', err});
+            console.warn("Get failed...", err)
+            res.status(500).json({message : 'Failed to find cars, check console.'});
         })
+})
+router.post('/', (req, res) => {
+    //The data
+    const carData = req.body;
+    db('cars').insert(carData)
+     .then(ids => {
+         db('cars').where({ id : ids[0]})
+         .then(newCarEntry => {
+             console.log("Stored data.")
+             res.status(201).json(newCarEntry);
+         });
+     })
+     //Handles any errors that may occur - will post error in console
+     .catch(err => {
+         console.warn("Post failed...", err)
+         res.status(500).json({ message: "Failed to store data, check console."})
+     })
 })
 module.exports = router;
