7ffc6b2c4df699f87d7f095bb462ca67c3babc3f
Ben <allenben@protonmail.com>
2019-09-11 04:16:52 -0500

    Server running - untested

---
+++ cars/cars-router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const db = require("../data/db-config");
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    db('cars')
+        .then(cars => {
+            res.json(cars);
+        })
+        .catch(err => {
+            res.status(500).json({message : 'Failed to find cars:', err});
+        })
+})
+module.exports = router;
