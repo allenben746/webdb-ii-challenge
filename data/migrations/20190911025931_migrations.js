4574c3301128520b142c9a25cecfbb778175b607
Ben <allenben@protonmail.com>
2019-09-11 03:04:02 -0500

    Boilerplate knex schema implemented

--- data/migrations/20190911025931_migrations.js
+++ data/migrations/20190911025931_migrations.js
@@ -1,6 +1,19 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('cars', tbl => {
+  //Create VIN (id)
+
+  //Create make
+
+  //Create model
+
+  //Create mileage
+
+  //Transmission type
+
+  //Title Status
+
+    })
 };
 
 exports.down = function(knex) {
